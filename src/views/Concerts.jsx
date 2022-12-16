fd914af44338ae560af304086959bde65be2857b
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-12-16 19:50:37 -0300

    Cambiado error con clases

--- src/views/Concerts.jsx
+++ src/views/Concerts.jsx
@@ -1,12 +1,10 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import { Text, View } from "react-native";
+import React from "react";
 
-export default class Concerts extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Concerts</Text>
-      </View>
-    )
-  }
+export default function Concerts() {
+  return (
+    <View>
+      <Text>Concerts</Text>
+    </View>
+  );
 }
